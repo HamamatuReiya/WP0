fe47128c28dd2d1079fe148f27ed199a32a2c246
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2024-08-31 20:15:03 +0900

    8月

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public Rigidbody rd;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Screen.SetResolution(1920, 1080, false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        const float moveSpeed = 5.0f;
+        Vector3 v = rd.velocity;
+        if (Input.GetKey(KeyCode.D))
+        {
+            v.x = moveSpeed;
+            //transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            v.x = -moveSpeed;
+            //transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            v.z = moveSpeed;
+            //transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            v.z = -moveSpeed;
+            //transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        rd.velocity = v;
     }
 }
