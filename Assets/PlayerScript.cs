a4b03f8914d27eb12386168e4370daad73c7dcbc
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2024-11-03 21:16:23 +0900

    10月

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public Rigidbody rd;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(1920, 1080, false);
+        
     }
 
     // Update is called once per frame
@@ -16,26 +17,36 @@ public class PlayerScript : MonoBehaviour
     {
         const float moveSpeed = 5.0f;
         Vector3 v = rd.velocity;
+        
+        animator.SetBool("mode", false);
         v.x = 0;
         v.z = 0;
         if (Input.GetKey(KeyCode.D))
         {
             v.x = moveSpeed;
+            animator.SetBool("mode", true);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
             //transform.rotation = Quaternion.Euler(0, 90, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
             v.x = -moveSpeed;
+            animator.SetBool("mode", true);
+            transform.rotation = Quaternion.Euler(0, -90, 0);
             //transform.rotation = Quaternion.Euler(0, -90, 0);
         }
         if (Input.GetKey(KeyCode.W))
         {
             v.z = moveSpeed;
+            animator.SetBool("mode", true);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
             //transform.rotation = Quaternion.Euler(0, 90, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
             v.z = -moveSpeed;
+            animator.SetBool("mode", true);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
             //transform.rotation = Quaternion.Euler(0, -90, 0);
         }
         rd.velocity = v;
--- Assets/SwitchScript.cs
+++ Assets/SwitchScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class SwitchScript : MonoBehaviour
 {
+    public GameObject Goal;
     // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Goal.SetActive(true);
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
